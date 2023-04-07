63a273def3b3d5a2dc3e486a48609e892aae2f5d
Byrontosh <by_tosh20@hotmail.com>
2023-04-07 09:21:22 -0500

    Request

--- src/index.js
+++ src/index.js
@@ -1,20 +1,28 @@
 // const express = require('express')
 // const app = express()
-// app.get('/',(req,res)=>{
-//     res.send("Landing page")
+// app.use(express.json())
+// app.post('/product',(req,res)=>{
+//     const {nombre,edad} = req.body
+//     res.send(`Los datos enviados son: ${nombre} - ${edad}`)
 // })
-// app.get('/dashboard',(req,res)=>{
-//     res.send("Dashboard principal")
+// app.listen(3000)
+// console.log('Servidor ejecutandose en el puerto 3000')
+
+
+
+
+
+// const express = require('express')
+// const app = express()
+// app.get('/user/:data',(req,res)=>
+// {
+//     console.log(typeof(req.params.data));
+//     res.send(`Bienvenido - ${req.params.data}`)
 // })
-// app.get('/contact',(req,res)=>{
-//     res.send("Contactos de la página web")
-// })
-// app.get('/products',(req,res)=>{
-//     res.send("Productos del e-commerce")
-// })
-// // Manejo de una ruta que no sea encontrada
-// app.use((req,res)=>{
-//     res.status(404).send("Página no encontrada - 404")
+// app.get('/operacion/:num1/:num2',(req,res)=>
+// {
+//     const {num1,num2}= req.params
+//     res.send(`La suma es - ${Number(num1) + Number(num2) }`)
 // })
 // app.listen(3000)
 // console.log('Servidor ejecutandose en el puerto 3000');
@@ -23,72 +31,23 @@
 
 
 
-
 // const express = require('express')
 // const app = express()
-// app.get('/',(req,res)=>{
-//     res.send("Obtener datos del servidor")
-// })
-// app.post('/',(req,res)=>{
-//     res.send("Enviar datos al servidor")
-// })
-// app.put('/',(req,res)=>{
-//     res.send("Actualizar toda la información")
-// })
-// app.patch('/',(req,res)=>{
-//     res.send("Actualizar solo un porción de la información")
-// })
-// app.delete('/',(req,res)=>{
-//     res.send("Eliminar algún dato del servidor")
-// })
-// app.listen(3000)
-// console.log('Servidor ejecutandose en el puerto 3000');
-
-
-
-
-// const express = require('express')
-// const app = express()
-// app.get('/',(req,res)=>{
+// app.get('/user/:profile/photo',(req,res)=>
+// {
+//     if(req.params.profile === "byron")
 //     res.sendFile('./girlandboy.png',{
 //         root:__dirname
 //     })
+//     else{
+//         res.send("No es un usuario válido...")
+//     }
 // })
-// console.log(__dirname);
-// app.listen(3000)
-// console.log('Servidor ejecutandose en el puerto 3000');
-
-
-
-
-// const express = require('express')
-// const app = express()
-// app.get('/user',(req,res)=>{
-//     res.json({
-//         "nombre":"Byron",
-//         "apellido":"Loarte",
-//         "edad":34,
-//         "direccion":{
-//             "callePrincipal":"Quitumbe",
-//             "calleSecundaria":"Calle B",
-//         }
-//     })
+// app.get('/nombre/:nombre/edad/:edad',(req,res)=>{
+//     res.send(`El nombre del usuario es: ${req.params.nombre} y su edad es: ${req.params.edad}`)
 // })
 // app.listen(3000)
-// console.log('Servidor ejecutandose en el puerto 3000');
-
-
-
-
-
-// const express = require('express')
-// const app = express()
-// app.get('/isAlive',(req,res)=>{
-//     res.sendStatus(201)
-// })
-// app.listen(3000)
-// console.log('Servidor ejecutandose en el puerto 3000');
-
+// console.log('Servidor ejecutandose en el puerto 3000')
 
 
 
@@ -97,20 +56,17 @@
 
 // const express = require('express')
 // const app = express()
-// const {engine} = require('express-handlebars');
-// app.engine('.hbs',engine({
-//     extname:'.hbs'
-// }))
-// app.set('view engine', '.hbs');
-// app.set('views', './src/views');
-// app.get('/',(req,res)=>{
-//     res.render('home')
+
+// app.get('/search',(req,res)=>{
+//     console.log(req.query)
+//     if(req.query.data === "js")
+//     {
+//         res.send("Libros de JS")
+//     }
+//     else{
+//         res.send("Libros de dom")
+//     }
 // })
-// app.get('/login',(req,res)=>{
-//     res.render('login')
-// })
-// app.get('/personal',(req,res)=>{
-//     res.render('personal')
-// })
+
 // app.listen(3000)
 // console.log('Servidor ejecutandose en el puerto 3000');
